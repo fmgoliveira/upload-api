e5a13826cb42d32d1028d7fffd6dd7018180cecb
Francisco Oliveira <francisco.m.gomes.oliveira@gmail.com>
2023-08-22 12:12:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
+var cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 const { appUrl, appPort } = require('./config.json');
 
